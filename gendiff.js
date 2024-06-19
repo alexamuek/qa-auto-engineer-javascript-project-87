f6235ae867c75aeb48cfc9549a32d71f1cc94b30
Alex <alexamuek@gmail.com>
2024-06-19 20:16:55 +0800

    init project, add gendiff.js

---
+++ gendiff.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .parse(process.argv);
