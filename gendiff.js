4fa9c985d838a800e3144eff6f1eecdd2737c960
Alex <alexamuek@gmail.com>
2024-06-20 09:32:26 +0800

    add options and arguments

--- gendiff.js
+++ gendiff.js
@@ -4,5 +4,8 @@ const { program } = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
