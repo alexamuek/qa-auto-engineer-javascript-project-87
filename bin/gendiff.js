9aea932e075fdc62b278f3f83e83e23f39af2f45
Alex <alexamuek@gmail.com>
2024-07-02 08:26:13 +0800

    update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,13 +2,19 @@
 import { program } from 'commander';
 import compare from './compare.js';
 
-program
-  .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2, options) => {
-    console.log(compare(filepath1, filepath2, options.format));
-  })
-  .parse(process.argv);
+const gendiff = () => {
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .version('1.0.0')
+    .option('-f, --format <type>', 'output format')
+    .action((filepath1, filepath2, options) => {
+      console.log(compare(filepath1, filepath2, options.format));
+    })
+    .parse(process.argv);
+};
+
+gendiff();
+
+export default gendiff;
