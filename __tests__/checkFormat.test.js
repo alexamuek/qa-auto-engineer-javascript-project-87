c9efdb0e51e7b4d3b3f652df722d389aad5965b6
Alex <alexamuek@gmail.com>
2024-07-02 23:27:16 +0800

    switch off logs

--- __tests__/checkFormat.test.js
+++ __tests__/checkFormat.test.js
@@ -35,8 +35,8 @@ test('functional test, default format', () => {
 });
 
 test('functional test, stylish format', () => {
-  const path1 = getFixturePath('file1.json');
-  const path2 = getFixturePath('file2.json');
+  const path1 = getFixturePath('file1.yml');
+  const path2 = getFixturePath('file2.yml');
   const result = compare(path1, path2, 'stylish');
   const preparedResult = result.replaceAll('\n', '');
   const expectedResult = '{  - follow: false    host: hexlet.io  - proxy: 123.234.53.22  - timeout: 50  + timeout: 20  + verbose: true}';
