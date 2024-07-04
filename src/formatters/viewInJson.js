873c973f83f8705feeaef6aaaefd10b5d3ddb5a4
Alex <alexamuek@gmail.com>
2024-07-04 23:26:14 +0800

    update json formatter

--- src/formatters/viewInJson.js
+++ src/formatters/viewInJson.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 
 const formatToJson = (arr) => {
   const filteredLines = arr.filter((item) => item.status !== 'unchanged');
-  const lines = filteredLines.map((item) => [item.key, item.value]);
-  const obj = _.fromPairs(lines);
+  const arr1 = filteredLines.filter((item) => item.file === 'file1');
+  const arr2 = filteredLines.filter((item) => item.file === 'file2');
+  const obj = _.fromPairs([['file1', arr1], ['file2', arr2]]);
   const result = JSON.stringify(obj);
   return result;
 };
