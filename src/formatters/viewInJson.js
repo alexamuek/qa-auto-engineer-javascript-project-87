f889c162b59ecd38b0ed8af439845c78228d5904
Alex <alexamuek@gmail.com>
2024-07-04 23:05:43 +0800

    fix formatInJson

--- src/formatters/viewInJson.js
+++ src/formatters/viewInJson.js
@@ -3,7 +3,6 @@ import _ from 'lodash';
 const formatToJson = (arr) => {
   const filteredLines = arr.filter((item) => item.status !== 'unchanged');
   const lines = filteredLines.map((item) => [item.key, item.value]);
-  console.log(lines);
   const obj = _.fromPairs(lines);
   const result = JSON.stringify(obj);
   return result;
