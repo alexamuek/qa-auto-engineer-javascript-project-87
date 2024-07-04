8cc6bb24689fe3522a921f08bf995da18c9460bd
Alex <alexamuek@gmail.com>
2024-07-04 21:34:26 +0800

    update viewInJson

--- src/formatters/viewInJson.js
+++ src/formatters/viewInJson.js
@@ -1,16 +1,9 @@
+import _ from 'lodash';
 import getLines from './helpers.js';
 
-const addToObject = (acc, arr) => {
-  const newLine = {};
-  const [key, value] = arr;
-  newLine[key] = value;
-  const result = { ...acc, ...newLine };
-  return result;
-};
-
 const formatToJson = (arr) => {
-  const lines = getLines(arr, 'json');
-  const obj = lines.reduce(addToObject, {});
+  const lines = arr.map((item) => [item.key, item.value]);
+  const obj = _.fromPairs(lines);
   const result = JSON.stringify(obj);
   return result;
 };
