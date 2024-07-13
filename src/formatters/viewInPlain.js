00cea062d9b3922a7e2ee0ad1cf984fbfe678a2f
Alex <alexamuek@gmail.com>
2024-07-13 12:50:47 +0800

    update viewInPlain.js

--- src/formatters/viewInPlain.js
+++ src/formatters/viewInPlain.js
@@ -5,14 +5,11 @@ const formatToPlain = (sortedLines) => {
     removed: (node) => `Property '${node.key}' was removed`,
     changed: (node) => `Property '${node.key}' was updated. From ${node.previousValue} to ${node.value}`,
   };
-  const result = sortedLines.reduce((acc, item) => {
-    const newLine = funcs[item.status](item);
-    if (newLine !== '') {
-      return `${acc}${newLine}\n`;
-    }
-    return acc;
-  }, '');
-  return result.slice(0, -1);
+  const result = sortedLines
+    .map((item) => funcs[item.status]?.(item))
+    .filter(Boolean)
+    .join('\n');
+  return result;
 };
 
 export default formatToPlain;
